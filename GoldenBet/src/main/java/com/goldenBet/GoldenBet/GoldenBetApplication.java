a676c244bd165467e2ab4c6889fb47faceb9e7a0
musanchez <111673304+musanchez@users.noreply.github.com>
2023-09-30 19:39:33 -0600

    Add files via upload

---
+++ GoldenBet/src/main/java/com/goldenBet/GoldenBet/GoldenBetApplication.java
@@ -0,0 +1,13 @@
+package com.goldenBet.GoldenBet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenBetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenBetApplication.class, args);
+	}
+
+}
